46a67d0b849fe829920a7abc6b9c6b419913405e
marekgacek45 <marekgacek45@gmail.com>
2023-03-17 08:51:26 +0100

    add footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,17 @@
-<h3>footer</h3>
+<footer>
+    <div>
+        <p>&copy; <span></span> GameSiteCMS</p>
+        <div>
+            <p>created by <a href="https://github.com/gacmen45"> <img src="../assets/gacmen.png"
+                        alt="Mareg Gacek logo">Marek 'gacmen45' Gacek</a>
+        </div>
+    </div>
+</footer>
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+    crossorigin="anonymous"></script>
 </body>
 
 </html>
